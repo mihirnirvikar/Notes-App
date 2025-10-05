6e6db8fbf9ffa78b0ac9dce7ce73c843101d0724
Mihir Nirvikar <mihirnirvikar3@gmail.com>
2025-10-05 18:46:41 +0530

    feat: create and style note form

--- backend/public/script.js
+++ backend/public/script.js
@@ -1,5 +1,27 @@
-const addNotes = document.querySelector(".addNotes");
+const addNotes = document.querySelector('.addNotes');
+const popupForm = document.querySelector('.form-popup-card');
+const popupCloseBtn = document.querySelector('.popup-close-btn');
 
-addNotes.addEventListener("click", () => {
-    console.log("Clicked");
+
+
+const dateInput = document.getElementById('validationCustom04');
+const today = new Date().toISOString().split('T')[0];
+dateInput.value = today;
+
+addNotes.addEventListener('click', () => {
+    popupForm.style.display = 'block';
 })
+
+popupCloseBtn.addEventListener('click', (e) => {
+    popupForm.style.display = 'none';
+})
+
+document.addEventListener('click', (e) => {
+  if (
+    popupForm.style.display === 'block' &&
+    !popupForm.contains(e.target) &&
+    !addNotes.contains(e.target)
+  ) {
+    popupForm.style.display = 'none';
+  }
+});
